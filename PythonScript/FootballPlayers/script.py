d4d8bac11f338bf9461fd08c18e24b62ed438029
nurisc2 <nikola.ursic@fer.hr>
2020-10-19 23:56:25 +0200

    Added getById, put & delete methods in Python script

--- PythonScript/FootballPlayers/script.py
+++ PythonScript/FootballPlayers/script.py
@@ -11,10 +11,9 @@ def position(i):
     }
     return switcher.get(i, "Benchwarmer")
 
-
 URL = "http://localhost:8080/api/footballPlayers"
 
-# punjenje baze podatcima
+# kreiranje igraca
 for i in range(1000):
     data = {'FirstName': 'Name' + str(i),
             'LastName': 'Surname' + str(i),
@@ -22,7 +21,7 @@ for i in range(1000):
             'FieldPosition': position(i % 4)}
     r = requests.post(url=URL, data=data)
 
-# dohvat podataka i spremanje
+# dohvat svih podataka i spremanje
 r = requests.get(url=URL)
 data = r.json()
 csvFile = open('E:\export.csv', 'w', newline='', encoding='utf-8')  # TODO namjestit
@@ -33,3 +32,24 @@ writer.writeheader()
 for d in data:
     writer.writerow({'Id': d['Id'], 'FirstName': d['FirstName'], 'LastName': d['LastName'], 'Age': d['Age'],
                      'FieldPosition': d['FieldPosition']})
+
+# dohvat jednog podataka i spremanje
+playerId = 1;  # postavljanje kojeg igraca zelimo dohvatit
+r = requests.get(url=URL + '/' + str(playerId))
+data = r.json()
+csvFile = open('E:\exportOnePlayer.csv', 'w', newline='', encoding='utf-8')  # TODO namjestit
+writer = csv.DictWriter(csvFile, delimiter=',', fieldnames=columns)
+writer.writeheader()
+writer.writerow({'Id': data['Id'], 'FirstName': data['FirstName'], 'LastName': data['LastName'], 'Age': data['Age'],
+                 'FieldPosition': data['FieldPosition']})
+# update igraca
+data = {'Id': 1,
+        'FirstName': 'NewName',
+        'LastName': 'NewSurname',
+        'Age': 20,
+        'FieldPosition': 'Forward'}
+r = requests.put(url=URL, data=data)
+
+# brisanje igraca
+playerId = 2;  # postavljanje kojeg igraca zelimo obrisati
+r = requests.delete(url=URL + '/' + str(playerId))
